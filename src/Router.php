d6bb6410a194eb51724527ce31dc951c749e0dd0
jsstoni <jsstoniha@gmail.com>
2021-11-06 21:26:21 -0300

    url host

--- src/Router.php
+++ src/Router.php
@@ -5,8 +5,13 @@
 namespace src;
 class Router
 {
+	private	$REQUEST_URL,
+			$HOST_URL;
+
 	public function __construct()
 	{
+		$this->REQUEST_URL = $_SERVER['REQUEST_URI'];
+		$this->HOST_URL = $_SERVER['HTTP_HOST'];
 	}
 }
 ?>
