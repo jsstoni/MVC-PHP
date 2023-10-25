83237b64745acdd6832e5594f235c450c7c34dcd
jsstoni <jsstoniha@gmail.com>
2023-10-25 16:18:49 -0300

    response json

--- src/Reponse.php
+++ src/Reponse.php
@@ -12,6 +12,8 @@ class Response
 
     public function json($obj = [])
     {
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($obj);
     }
 
     public function send($any = "")
