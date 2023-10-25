1a92a0f9572d587ea2109840b6c99ac2d70fbe74
jsstoni <jsstoniha@gmail.com>
2023-10-25 16:18:21 -0300

    class response

---
+++ src/Reponse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class Response
+{
+    public function status($statusCode)
+    {
+    }
+
+    public function json($obj = [])
+    {
+    }
+
+    public function send($any = "")
+    {
+    }
+
+    public function render($template, $data = [])
+    {
+    }
+}
