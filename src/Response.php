a867a817caaa525942a562437b85ec40d687a534
jsstoni <jsstoniha@gmail.com>
2023-10-25 19:19:17 -0300

    Response types

--- src/Response.php
+++ src/Response.php
@@ -6,24 +6,24 @@ use League\Plates\Engine as Engine;
 
 class Response
 {
-    public function status($statusCode)
+    public function status(int $statusCode): self
     {
         http_response_code($statusCode);
         return $this;
     }
 
-    public function json($obj = [])
+    public function json(array $obj = []): void
     {
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($obj);
     }
 
-    public function send($any = "")
+    public function send(string $any = ""): void
     {
         echo $any;
     }
 
-    public function render($path, $data = [])
+    public function render(string $path, array $data = []): void
     {
         $tpl = new Engine('app/views');
         $tpl->addFolder("layouts", "app/views/layouts");
