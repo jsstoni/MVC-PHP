8004e632283fc6a84fc53bac104d7f48f1d3b677
jsstoni <jsstoniha@gmail.com>
2023-10-27 13:49:40 -0300

    set environment variables

--- src/DataBase.php
+++ src/DataBase.php
@@ -6,10 +6,10 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver' => 'mysql',
-    'host' => 'localhost',
-    'database' => 'projects',
-    'username' => 'root',
-    'password' => '',
+    'host' => isset($_ENV['HOST']) ? $_ENV['HOST'] : 'localhost',
+    'database' => isset($_ENV['DATABASE']) ? $_ENV['DATABASE'] : 'projects',
+    'username' => isset($_ENV['USERNAME']) ? $_ENV['USERNAME'] : 'root',
+    'password' => isset($_ENV['PASSWORD']) ? $_ENV['PASSWORD'] : '',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
