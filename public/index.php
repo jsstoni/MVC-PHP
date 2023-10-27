b08559d2351486b21e44d3c35d8e2fcf7e0a6776
jsstoni <jsstoniha@gmail.com>
2023-10-27 19:59:57 -0300

    creating entry point from server to public

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 use App\Router\Router;
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 Router::create('/');
