5961b9be92746ca6e229dfa0b5edb2138217b06e
jsstoni <jsstoniha@gmail.com>
2023-10-27 20:59:33 -0300

    section css, js

--- app/views/layouts/base.php
+++ app/views/layouts/base.php
@@ -6,10 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $this->e($title); ?></title>
     <link rel="stylesheet" href="<?= $this->asset('/css/app.css') ?>" />
+    <style>
+        <?= $this->section('css'); ?>
+    </style>
 </head>
 
 <body>
-    <?php echo $this->section('content'); ?>
+    <?= $this->section('content'); ?>
+    <script>
+        <?= $this->section('js'); ?>
+    </script>
 </body>
 
 </html>
