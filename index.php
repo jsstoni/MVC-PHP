35508a9323787212e3f09c37691a589a1fe8a632
jsstoni <jsstoniha@gmail.com>
2023-10-27 13:46:39 -0300

    dotenv

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require 'vendor/autoload.php';
 
 use App\Router\Router;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 Router::create('/');
 Router::run();
